4d96d3754ec79262515e6970ae17a4b45eb98cc5
jonghopark95 <jong951005@gmail.com>
2021-07-14 18:57:46 +0900

    style : 메인 페이지 네비게이션 바 styling

--- client/src/views/page-components/MainPage/MainPageNavBar/MainPageNavBar.js
+++ client/src/views/page-components/MainPage/MainPageNavBar/MainPageNavBar.js
@@ -1,14 +1,16 @@
+import { CATEGORY_ICON, LOCATION_ICON, MENU_ICON, USER_ICON } from '../../../../constants/imagePath.js';
+
 export default function MainPageNavBar({ $selector, onItemClick }) {
   this.$selector = $selector;
 
   this.render = () => {
     this.$selector.innerHTML = `
         <nav class="nav">
-            <img class="category-img" src="../../public/images/icons/category.svg">
-            <img class="user-img" src="../../public/images/icons/user.svg">
-            <img class="menu-img" src="../../public/images/icons/menu.svg">
+            <img class="category-img" src=${CATEGORY_ICON}>
+            <img class="user-img" src=${USER_ICON}>
+            <img class="menu-img" src=${MENU_ICON}>
             <div class="location-wrap">
-                <img class="location-wrap__img" src="../../public/images/icons/location.svg">
+                <img class="location-wrap__img" src=${LOCATION_ICON}>
                 <span class="location-wrap__title">역삼동</span>
             </div>
         </nav>
