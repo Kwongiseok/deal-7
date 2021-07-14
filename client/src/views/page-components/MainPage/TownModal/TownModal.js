93a6912973b1c7ceb4261a13951638ee089be7cc
jonghopark95 <jong951005@gmail.com>
2021-07-15 01:01:26 +0900

    feat : Town Modal 로직 구현

---
+++ client/src/views/page-components/MainPage/TownModal/TownModal.js
@@ -0,0 +1,40 @@
+import { createDOMwithSelector } from '../../../../utils/createDOMwithSelector.js';
+
+export default function TownModal({ $selector, setTownModalOpenedState, setCurrentlyOpenedSlide }) {
+  this.$TownModal = createDOMwithSelector('aside', '.town-modal');
+  this.$selector = $selector;
+  this.$selector.appendChild(this.$TownModal);
+
+  this.showTownModal = (isTownModalOpened) => {
+    if (!isTownModalOpened) {
+      return this.$TownModal.classList.remove('opened');
+    }
+
+    return this.$TownModal.classList.add('opened');
+  };
+
+  this.$TownModal.addEventListener('click', (e) => {
+    e.stopPropagation();
+
+    if (e.target.closest('[data-slide-item=town]')) {
+      setTownModalOpenedState('close');
+      setCurrentlyOpenedSlide('town');
+      return;
+    }
+
+    // TODO: Change town state on Main Page
+  });
+
+  this.render = () => {
+    this.$TownModal.innerHTML = `
+            <div class='town-modal__item'>
+                <span>역삼동</span>
+            </div>
+            <div class='town-modal__item' data-slide-item="town">
+                <span>내 동네 설정하기</span>
+            </div>
+        `;
+  };
+
+  this.render();
+}
