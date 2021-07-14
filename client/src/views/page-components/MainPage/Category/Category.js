8452ced81e1f71531828d0e9c12492e8d4d9350f
jonghopark95 <jong951005@gmail.com>
2021-07-15 04:26:54 +0900

    chore : Choiced 되었을 경우 class 추가하는 로직 추가

--- client/src/views/page-components/MainPage/Category/Category.js
+++ client/src/views/page-components/MainPage/Category/Category.js
@@ -1,14 +1,22 @@
 import { createDOMwithSelector } from '../../../../utils/createDOMwithSelector.js';
 
-export default function Category({ $selector, infos }) {
+export default function Category({ $selector, infos: { category, isChoiced } }) {
   this.$CategoryGridItemWrap = createDOMwithSelector('div', '.category-grid__item-wrap');
+  this.$CategoryGridItemWrap.setAttribute('data-category', category);
   $selector.appendChild(this.$CategoryGridItemWrap);
 
   this.render = () => {
     this.$CategoryGridItemWrap.innerHTML = `
-        <span>${infos}</span>
+        <span data-category='${category}'>${category}</span>
     `;
+
+    setChoicedClassOnTarget(this.$CategoryGridItemWrap, isChoiced);
   };
 
   this.render();
 }
+
+const setChoicedClassOnTarget = ($target, isChoiced) => {
+  if (!isChoiced) return $target.classList.remove('choiced');
+  return $target.classList.add('choiced');
+};
