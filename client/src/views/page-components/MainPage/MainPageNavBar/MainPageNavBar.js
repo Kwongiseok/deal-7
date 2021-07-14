5d359f11fc24ebac5ca4ce0dfd918994850a43d3
jonghopark95 <jong951005@gmail.com>
2021-07-15 01:02:16 +0900

    fix : Nav 바의 아이템들이 같은 속성을 공유하고 있어, 이를 수정

--- client/src/views/page-components/MainPage/MainPageNavBar/MainPageNavBar.js
+++ client/src/views/page-components/MainPage/MainPageNavBar/MainPageNavBar.js
@@ -9,9 +9,9 @@ export default function MainPageNavBar({ $selector }) {
             <img class="category-img" src=${CATEGORY_ICON} data-slide-item="category">
             <img class="user-img" src=${USER_ICON} data-slide-item="user">
             <img class="menu-img" src=${MENU_ICON} data-slide-item="menu">
-            <div class="location-wrap" data-slide-item="town">
-                <img class="location-wrap__img" src=${LOCATION_ICON} data-slide-item="town">
-                <span class="location-wrap__title" data-slide-item="town">역삼동</span>
+            <div class="location-wrap" data-item="set-town">
+                <img class="location-wrap__img" src=${LOCATION_ICON}>
+                <span class="location-wrap__title">역삼동</span>
             </div>
         </nav>
     `;
