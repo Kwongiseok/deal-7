fed681add77be059e85074fd2ef897c537b01218
jonghopark95 <jong951005@gmail.com>
2021-07-14 18:58:19 +0900

    feat : Product 만들기 버튼 로직 구현 및 스타일링

---
+++ client/src/views/page-components/MainPage/CreateProductButton/CreateProductButton.js
@@ -0,0 +1,16 @@
+import { ADD_ICON } from '../../../../constants/imagePath.js';
+import { createDOMwithSelector } from '../../../../utils/createDOMwithSelector.js';
+
+export default function CreateProductButton({ $selector }) {
+  this.$AddProductBtn = createDOMwithSelector('div', '.add-product-btn');
+  this.$selector = $selector;
+  this.$selector.appendChild(this.$AddProductBtn);
+
+  this.render = () => {
+    this.$AddProductBtn.innerHTML = `
+            <img class="add-img" src=${ADD_ICON}>
+        `;
+  };
+
+  this.render();
+}
