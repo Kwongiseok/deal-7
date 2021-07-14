12bb9ef1904f0cf03f88791c499253ba84351f53
Kwongiseok <ri2377@naver.com>
2021-07-15 01:23:34 +0900

    feat : carousel 노드 복사

--- client/src/views/page-components/ProductDetailPage/ProductDetail/Carousel/Carousel.js
+++ client/src/views/page-components/ProductDetailPage/ProductDetail/Carousel/Carousel.js
@@ -1,6 +1,130 @@
 export default function Carousel({ $target }) {
+  this.images = [
+    'https://i.pinimg.com/564x/d7/ec/75/d7ec75c9e68873ee75b734ac4ab09ced.jpg',
+    'https://i.pinimg.com/474x/30/5a/21/305a216481dfaaec10fd59cf1f667652.jpg',
+    'https://i.pinimg.com/474x/5f/a2/8e/5fa28eae2bdebd6ab2d30690304927b9.jpg',
+    'https://i.pinimg.com/474x/a6/56/70/a65670944d4bf492a3a71c4a95bb3910.jpg',
+    'https://i.pinimg.com/474x/e8/58/f3/e858f330363c0fb4240ca8cad087f74d.jpg',
+    'https://i.pinimg.com/474x/c5/eb/47/c5eb47f58dd27a764f88551151f54893.jpg',
+  ];
   this.$carousel = document.createElement('div');
   this.$carousel.className = 'carousel__container';
-  this.$carousel.innerHTML = `<img class="carousel__img" src="https://i.pinimg.com/564x/d7/ec/75/d7ec75c9e68873ee75b734ac4ab09ced.jpg"/>`;
+  this.$slides = document.createElement('div');
+  this.$slides.className = 'carousel__slides';
+  this.slideArr = this.images.map((src) => {
+    const $slide = document.createElement('img');
+    $slide.src = src;
+    $slide.className = 'carousel__img';
+    return $slide;
+  });
+
+  this.slideArr.forEach((slide) => this.$carousel.appendChild(slide));
   $target.appendChild(this.$carousel);
+
+  this.slide = (wrapper, items) => {
+    let posX1 = 0;
+    let posX2 = 0;
+    let posInitial,
+      posFinal,
+      threshold = 100;
+    let slidesLength = this.images.length;
+    let firstSlide = this.slideArr[0];
+    let lastSlide = this.slideArr[this.slideArr.length - 1];
+    let cloneFirst = firstSlide.cloneNode(true);
+    let cloneLast = lastSlide.cloneNode(true);
+    let index = 0;
+    let allowShift = true;
+
+    items.appendChild(cloneFirst);
+    items.insertBefore(cloneLast, firstSlide);
+
+    wrapper.classList.add('loaded');
+
+    // Touch events
+    items.addEventListener('touchstart', dragStart);
+    items.addEventListener('touchend', dragEnd);
+    items.addEventListener('touchmove', dragAction);
+
+    // Transition events
+    items.addEventListener('transitionend', checkIndex);
+
+    function dragStart(e) {
+      e = e || window.event;
+      e.preventDefault();
+      posInitial = items.offsetLeft;
+
+      if (e.type == 'touchstart') {
+        posX1 = e.touches[0].clientX;
+      } else {
+        posX1 = e.clientX;
+        document.onmouseup = dragEnd;
+        document.onmousemove = dragAction;
+      }
+    }
+
+    function dragAction(e) {
+      e = e || window.event;
+
+      if (e.type == 'touchmove') {
+        posX2 = posX1 - e.touches[0].clientX;
+        posX1 = e.touches[0].clientX;
+      } else {
+        posX2 = posX1 - e.clientX;
+        posX1 = e.clientX;
+      }
+      items.style.left = items.offsetLeft - posX2 + 'px';
+    }
+
+    function dragEnd(e) {
+      posFinal = items.offsetLeft;
+      if (posFinal - posInitial < -threshold) {
+        shiftSlide(1, 'drag');
+      } else if (posFinal - posInitial > threshold) {
+        shiftSlide(-1, 'drag');
+      } else {
+        items.style.left = posInitial + 'px';
+      }
+
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+
+    function shiftSlide(dir, action) {
+      items.classList.add('shifting');
+
+      if (allowShift) {
+        if (!action) {
+          posInitial = items.offsetLeft;
+        }
+
+        if (dir == 1) {
+          items.style.left = posInitial - slideSize + 'px';
+          index++;
+        } else if (dir == -1) {
+          items.style.left = posInitial + slideSize + 'px';
+          index--;
+        }
+      }
+
+      allowShift = false;
+    }
+
+    function checkIndex() {
+      items.classList.remove('shifting');
+
+      if (index == -1) {
+        items.style.left = -(slidesLength * slideSize) + 'px';
+        index = slidesLength - 1;
+      }
+
+      if (index == slidesLength) {
+        items.style.left = -(1 * slideSize) + 'px';
+        index = 0;
+      }
+
+      allowShift = true;
+    }
+  };
+
+  this.slide(this.$carousel, this.$slides);
 }
