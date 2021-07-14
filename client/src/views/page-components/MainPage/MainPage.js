f1c218f168c91fa74b8da4a4a1fef124cbd84bb3
jonghopark95 <jong951005@gmail.com>
2021-07-14 18:59:47 +0900

    feat : 메인 페이지에 사용될 로직 구현 및 스타일링

--- client/src/views/page-components/MainPage/MainPage.js
+++ client/src/views/page-components/MainPage/MainPage.js
@@ -1,23 +1,46 @@
-import UserSlide from "./UserSlide/UserSlide.js";
+import { SAMPLE_PRODUCTS_STATE } from '../../../dummy-file.js';
+import CreateProductButton from './CreateProductButton/CreateProductButton.js';
+import MainPageNavBar from './MainPageNavBar/MainPageNavBar.js';
+import ProductsWrap from './ProductsWrap/ProductsWrap.js';
 
-function MainPage(params) {
+/**
+ * currentlyopendSlide
+ * - 현재 오픈되어 있는 슬라이드 입니다.
+ * - 들어올 수 있는 값은 null | category | user | town | createProduct | menu 입니다.
+ */
+
+function MainPage() {
   this.state = {
-    name: "종호",
+    products: SAMPLE_PRODUCTS_STATE,
   };
-  this.$main = document.querySelector("#mainPage");
-
-  const userSlide = new UserSlide({
-    $main: this.$main,
-    onHandler: () => {
-      this.setState({ ...this.state, name: "기석" });
-    },
-    initialState: this.state.name,
-  });
 
   this.setState = (nextState) => {
-    this.state = nextState;
-    userSlide.setState({ name: this.state.name });
+    this.state = {
+      ...this.state,
+      ...nextState,
+    };
   };
+
+  const body = document.querySelector('#body');
+  this.$header = document.createElement('header');
+  this.$main = document.createElement('main');
+  this.$main.className = 'main';
+
+  body.appendChild(this.$header);
+  body.appendChild(this.$main);
+
+  this.$NavBar = new MainPageNavBar({
+    $selector: this.$header,
+  });
+
+  this.$ProductsWrap = new ProductsWrap({
+    $selector: this.$main,
+    products: this.state.products,
+  });
+
+  this.$CreateProductButton = new CreateProductButton({
+    $selector: body,
+  });
 }
 
 new MainPage();
