223dec14600854d0158c784b1106eb4fdb88a2dd
jonghopark95 <jong951005@gmail.com>
2021-07-15 01:00:45 +0900

    feat : Create product slide 로직 구현

---
+++ client/src/views/page-components/MainPage/CreateProductSlide/CreateProductSlide.js
@@ -0,0 +1,23 @@
+import { createDOMwithSelector } from '../../../../utils/createDOMwithSelector.js';
+
+export default function CreateProductSlide({ $selector }) {
+  this.$CreateProductSlide = createDOMwithSelector('div', '.create-product-slide');
+  this.$selector = $selector;
+  this.$selector.appendChild(this.$CreateProductSlide);
+
+  this.openCreateProductSlide = (currentlyOpenedSlide) => {
+    if (currentlyOpenedSlide !== 'create') {
+      return this.$CreateProductSlide.classList.remove('slide-trigerred');
+    }
+
+    return this.$CreateProductSlide.classList.add('slide-trigerred');
+  };
+
+  this.render = () => {
+    this.$CreateProductSlide.innerHTML = `
+      <h1>CREATE PRODUCT</h1>
+    `;
+  };
+
+  this.render();
+}
