be852688e95168cc3dcab15ceaf2bb62fe0105a2
jonghopark95 <jong951005@gmail.com>
2021-07-15 04:27:35 +0900

    chore : Category 가 변경되었을 시 자신이 choiced 되었는지 확인하는 키 값 추가

--- client/src/views/page-components/MainPage/CategorySlide/CategorySlide.js
+++ client/src/views/page-components/MainPage/CategorySlide/CategorySlide.js
@@ -2,9 +2,13 @@ import { CATEGORY_LIST } from '../../../../constants/categoryList.js';
 import { createDOMwithSelector } from '../../../../utils/createDOMwithSelector.js';
 import Category from '../Category/Category.js';
 
-export default function CategorySlide({ $selector }) {
+export default function CategorySlide({ $selector, setCategoryFilter }) {
   this.$CategorySlide = createDOMwithSelector('div', '.category-slide');
   $selector.appendChild(this.$CategorySlide);
+
+  this.state = {
+    currentCategory: null,
+  };
 
   this.openCategorySlide = (currentlyOpenedSlide) => {
     if (currentlyOpenedSlide !== 'category') {
@@ -14,7 +18,33 @@ export default function CategorySlide({ $selector }) {
     return this.$CategorySlide.classList.add('slide-trigerred');
   };
 
-  this.render = () => {
+  // state
+
+  this.setState = (nextState) => {
+    this.state = nextState;
+    this.checkCurrentCategory(this.state.currentCategory);
+  };
+
+  this.checkCurrentCategory = (currentCategory) => {
+    this.render(currentCategory);
+  };
+
+  // event
+  const bindEvents = () => {
+    this.$CategorySlide.addEventListener('click', (e) => {
+      const {
+        target: {
+          dataset: { category },
+        },
+      } = e;
+
+      if (!category) return;
+      setCategoryFilter(category);
+    });
+  };
+
+  // render;
+  this.render = (currentCategory) => {
     this.$CategorySlide.innerHTML = `
         <h2 class="category-slide__title">카테고리</h2>
         <div class="category-main">
@@ -23,14 +53,15 @@ export default function CategorySlide({ $selector }) {
         </div>
     `;
 
-    createCategoriesDOM(document.querySelector('.category-grid'));
+    createCategoriesDOM(document.querySelector('.category-grid'), currentCategory);
   };
 
   this.render();
+  bindEvents();
 }
 
-const createCategoriesDOM = ($selector) => {
+const createCategoriesDOM = ($selector, currentCategory) => {
   CATEGORY_LIST.map((category) => {
-    new Category({ $selector, infos: category });
+    new Category({ $selector, infos: { category, isChoiced: currentCategory === category } });
   });
 };
