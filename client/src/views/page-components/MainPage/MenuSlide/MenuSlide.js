7ceb2d2a1559ccb2dad996da2e52eae09f74b0fd
jonghopark95 <jong951005@gmail.com>
2021-07-15 22:57:29 +0900

    feat : 메뉴 슬라이드 클릭할 때마다 상태 변경 되도록 수정

--- client/src/views/page-components/MainPage/MenuSlide/MenuSlide.js
+++ client/src/views/page-components/MainPage/MenuSlide/MenuSlide.js
@@ -12,11 +12,71 @@ export default function MenuSlide({ $selector }) {
     return this.$MenuSlide.classList.add('slide-trigerred');
   };
 
+  //state
+  /**
+   * currentlyMenuSlideItem
+   * - 현재 MenuSlideItem이 보여주고 있는 값입니다.
+   * - 들어 올 수 있는 값은 'saleList' | 'chatList' | 'likeList' 입니다.
+   */
+  this.state = {
+    currentlyMenuSlideItem: 'saleList',
+  };
+
+  this.setState = (nextState) => {
+    this.state = { ...this.state, ...nextState };
+    this.render();
+  };
+
+  //Events
+  const bindEvents = () => {
+    document.addEventListener('click', ({ target }) => {
+      changeMenuSlideItemState(target);
+    });
+  };
+
+  /**
+   * 메뉴 아이템을 클릭할 때마다,
+   * 형식을 변경하고 (e.g. sale-list => saleList)
+   * 상태를 변경합니다.
+   */
+  const changeMenuSlideItemState = (target) => {
+    if (!target.dataset.menuSlideItem) return;
+
+    const [data, attribute] = target.dataset.menuSlideItem.split('-');
+    const convertedItem = `${data}${attribute[0].toUpperCase()}${attribute.slice(1)}`;
+
+    this.setState({ currentlyMenuSlideItem: `${convertedItem}` });
+  };
+
+  //render
   this.render = () => {
     this.$MenuSlide.innerHTML = `
-      <h1>MENU</h1>
+      ${getMenuSlideTitleDOM(this.state.currentlyMenuSlideItem)}
     `;
   };
 
   this.render();
+  bindEvents();
 }
+
+const getMenuSlideTitleDOM = (currentlyMenuSlideItem) => {
+  const DOM = {
+    saleList: `
+      <h2 class="menu-slide__title sale" data-menu-slide-item='sale-list'}>판매목록</h2>
+      <h2 class="menu-slide__title chat not-choiced" data-menu-slide-item='chat-list'}>채팅</h2>
+      <h2 class="menu-slide__title like not-choiced" data-menu-slide-item='like-list'}>관심목록</h2>
+    `,
+    chatList: `
+      <h2 class="menu-slide__title sale not-choiced" data-menu-slide-item='sale-list'}>판매목록</h2>
+      <h2 class="menu-slide__title chat" data-menu-slide-item='chat-list'}>채팅</h2>
+      <h2 class="menu-slide__title like not-choiced" data-menu-slide-item='like-list'}>관심목록</h2>
+    `,
+    likeList: `
+      <h2 class="menu-slide__title sale not-choiced" data-menu-slide-item='sale-list'}>판매목록</h2>
+      <h2 class="menu-slide__title chat not-choiced" data-menu-slide-item='chat-list'}>채팅</h2>
+      <h2 class="menu-slide__title like" data-menu-slide-item='like-list'}>관심목록</h2>
+    `,
+  };
+
+  return DOM[currentlyMenuSlideItem];
+};
