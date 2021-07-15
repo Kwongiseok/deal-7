d07d2196a2e5c01481b4cf8fe1271629d2b58282
jonghopark95 <jong951005@gmail.com>
2021-07-15 19:26:58 +0900

    chore : 이미지 파일들 경로 설정

--- client/src/constants/imagePath.js
+++ client/src/constants/imagePath.js
@@ -16,3 +16,11 @@ export const RIGHT_ICON = '/client/public/images/icons/right.svg';
 export const SEND_ICON = '/client/public/images/icons/send.svg';
 export const USER_ICON = '/client/public/images/icons/user.svg';
 export const DELETE_PHOTO_ICON = '/client/public/images/icons/deletePhoto.svg';
+
+// baedal2
+export const JOIN_WITH_ME = '/client/public/images/createdBaeminStyleImage/lets-with-me.png';
+export const JOIN_WITH_ME_2 = '/client/public/images/createdBaeminStyleImage/lets-with-me-2.png';
+export const LOGIN_BUTTON = '/client/public/images/createdBaeminStyleImage/login-btn.png';
+export const SIGNUP_BUTTON = '/client/public/images/createdBaeminStyleImage/signup-btn.png';
+export const UNDERLINE_BAEMIN_COLOR = '/client/public/images/createdBaeminStyleImage/underline-baemin-color.png';
+export const UNDERLINE_BLACK_COLOR = '/client/public/images/createdBaeminStyleImage/underline-black-color.png';
