80454f72d34890193adc66b8781359394f671c3b
Kwongiseok <ri2377@naver.com>
2021-07-15 22:06:43 +0900

    fix : formatPrice 가격 없을 떄는 달러표시 없애기

--- client/src/utils/formatPrice.js
+++ client/src/utils/formatPrice.js
@@ -1,5 +1,9 @@
 export default function formatPrice(price) {
   const number = price.replace(/[^0-9]/g, '');
   const convertPrice = number.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ',');
-  return `₩ ${convertPrice}`;
+  if (convertPrice) {
+    return `₩ ${convertPrice}`;
+  } else {
+    return '';
+  }
 }
--- client/src/views/page-components/ProductDetailPage/ProductDetailPage.js
+++ client/src/views/page-components/ProductDetailPage/ProductDetailPage.js
@@ -5,12 +5,12 @@ function ProductDetailPage() {
   const town = '역삼동';
   this.$ProductDetailPage = document.querySelector('#root');
 
-  const productDetailPage = new ProductDetail({ $target: this.$ProductDetailPage });
+  // const productDetail = new ProductDetail({ $target: this.$ProductDetailPage });
 
-  // new CreateProductSlide({
-  //   $target: this.$ProductDetailPage,
-  //   town,
-  // });
+  new CreateProductSlide({
+    $target: this.$ProductDetailPage,
+    town,
+  });
 }
 
 new ProductDetailPage();
