6018835af3c5051448952aa69c95bd287a6b5908
Kwongiseok <ri2377@naver.com>
2021-07-16 11:08:14 +0900

    style : 채팅이 긴 경우 생략 처리

--- client/src/views/page-components/ChatListPage/ChatRoomList/ChatRoomList.js
+++ client/src/views/page-components/ChatListPage/ChatRoomList/ChatRoomList.js
@@ -4,7 +4,7 @@ export default function ChatRoomList({ $target }) {
   // TODO 권기석 : 더미데이터, API 연동 후 수정할 예정입니다.
   this.state = {
     username: 'UserE',
-    lastChat: '실제로 신어볼 수 있는건가요??',
+    lastChat: '실제로 신어볼 수 있는건가요??ㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㄴㅇㅁㅇㄴㅁㄴㅇㅁㄴㅇㅇㄴㅁㅇ',
     unreadChats: 2,
     time: '1분 전',
     thumbnail: 'https://i.pinimg.com/564x/d7/ec/75/d7ec75c9e68873ee75b734ac4ab09ced.jpg',
