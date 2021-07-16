984326b72699b61323dd222723fdb2c8d1166e39
jonghopark95 <jong951005@gmail.com>
2021-07-16 11:22:09 +0900

    feat : TownSlide 내 동네 설정하는 로직 추가

--- client/src/views/page-components/MainPage/TownSlide/TownSlide.js
+++ client/src/views/page-components/MainPage/TownSlide/TownSlide.js
@@ -1,6 +1,9 @@
+import { DISTRICT_NOTIFICATION } from '../../../../constants/imagePath.js';
 import { createDOMwithSelector } from '../../../../utils/createDOMwithSelector.js';
+import TownSlideButton from '../TownSlideButton/TownSlideButton.js';
+import TownPlusButton from '../TownPlusButton/TownPlusButton.js';
 
-export default function TownSlide({ $selector }) {
+export default function TownSlide({ $selector, currentTown, towns, setTownFilter, setUserTown }) {
   this.$TownSlide = createDOMwithSelector('div', '.town-slide');
   $selector.appendChild(this.$TownSlide);
 
@@ -12,11 +15,51 @@ export default function TownSlide({ $selector }) {
     return this.$TownSlide.classList.add('slide-trigerred');
   };
 
+  //state
+  this.state = {
+    currentTown,
+    towns,
+  };
+
+  this.setState = (nextState) => {
+    this.state = { ...this.state, ...nextState };
+    this.render();
+    bindEvents();
+  };
+
+  //events
+  const bindEvents = () => {
+    this.$TownSlide.addEventListener('click', ({ target }) => {
+      if (target.classList.contains('town-slide-button')) {
+        setTownFilter({ town: target.innerText.trim() });
+      }
+      if (target.dataset.eraseItem) {
+        const eraseItem = target.dataset.eraseItem.split('-')[1];
+        const erasedTowns = this.state.towns.filter((elem) => elem !== eraseItem);
+        setUserTown({ town: erasedTowns, currentTownFilter: erasedTowns[0] });
+      }
+    });
+  };
+
+  //render
   this.render = () => {
     this.$TownSlide.innerHTML = `
-      <h1>TOWN</h1>
+      <h2 class="town-slide__title">내 동네 설정하기</h2>
+      <div class="town-screen">
+        <img src=${DISTRICT_NOTIFICATION} class="district__logo">
+        <div class="town-screen__buttons"></div>
+      </div>
     `;
+
+    const $TownScreen = document.querySelector('.town-screen__buttons');
+    this.state.towns.map((t) => {
+      new TownSlideButton({ $selector: $TownScreen, townName: t, isChoiced: t === this.state.currentTown });
+    });
+    if (this.state.towns.length < 2) {
+      new TownPlusButton({ $selector: $TownScreen });
+    }
   };
 
   this.render();
+  bindEvents();
 }
