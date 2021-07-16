bbffb11a25ef2d56c1a1813f5171a8dd857ec745
jonghopark95 <jong951005@gmail.com>
2021-07-16 11:21:14 +0900

    chore : Navigation bar 유저의 현재 town 을 갖도록 로직 수정

--- client/src/views/page-components/MainPage/MainPageNavBar/MainPageNavBar.js
+++ client/src/views/page-components/MainPage/MainPageNavBar/MainPageNavBar.js
@@ -1,6 +1,13 @@
 import { CATEGORY_ICON, LOCATION_ICON, MENU_ICON, USER_ICON } from '../../../../constants/imagePath.js';
 
-export default function MainPageNavBar({ $selector }) {
+export default function MainPageNavBar({ $selector, currentTown }) {
+  this.state = { currentTown };
+
+  this.setState = (nextState) => {
+    this.state = { ...this.state, ...nextState };
+    this.render();
+  };
+
   this.render = () => {
     $selector.innerHTML = `
         <nav class="nav">
@@ -9,7 +16,7 @@ export default function MainPageNavBar({ $selector }) {
             <img class="menu-img" src=${MENU_ICON} data-slide-item="menu">
             <div class="location-wrap" data-item="set-town">
                 <img class="location-wrap__img" src=${LOCATION_ICON}>
-                <span class="location-wrap__title">역삼동</span>
+                <span class="location-wrap__title">${this.state.currentTown}</span>
             </div>
         </nav>
     `;
