be69dd1a2a07507c45565f4100e0467e97ca43fe
jonghopark95 <jong951005@gmail.com>
2021-07-18 19:03:28 +0900

    chore : Body parser 코드 추가

--- server/server.js
+++ server/server.js
@@ -1,18 +1,22 @@
-const http = require("http");
-const express = require("express");
-const dotenv = require("dotenv");
-const cors = require("cors");
-const { promisePool: DB } = require("./db.js");
-dotenv.config();
+const http = require('http');
+const express = require('express');
+const cors = require('cors');
+
+const { pool } = require('./db.js');
+const authRouter = require('./router/auth.js');
+const productRouter = require('./router/product.js');
 
 const app = express();
 const server = http.createServer(app);
-
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
+app.use('/auth', authRouter);
+app.use('/product', productRouter);
+
 server.listen(PORT, () => {
-  console.log("server is running : ", PORT);
+  console.log('server is running : ', PORT);
 });
