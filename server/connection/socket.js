49551ad6180b17cf94c4a3b416f6de76be2f5e35
Kwongiseok <ri2377@naver.com>
2021-07-18 18:00:43 +0900

    feat : 같은 방인 사람에게만 보내기 기능

--- server/connection/socket.js
+++ server/connection/socket.js
@@ -27,13 +27,17 @@ let chats = [
   },
 ];
 
+let roomnumber = 1;
 class Socket {
   constructor(server) {
     this.wss = new Websocket.Server({
       server,
       verifyClient: (info, cb) => {
-        console.log(info.req.url.query);
-        info.req.user = Math.random() * 10;
+        const parsedUrl = info.req.url.split('/');
+        const [productId, buyerId] = [parsedUrl[1], parsedUrl[3]];
+        const roomId = parseInt(String(productId) + String(buyerId));
+        info.req.user = Math.random() * 10; // JWT 인증했던 것,
+        info.req.room = roomId;
         cb(true);
       },
     });
@@ -41,15 +45,17 @@ class Socket {
     // this.wss -> 소켓 서버 , ws -> 접속해온 클라이언트 소켓,
     this.wss.on('connection', (ws, req) => {
       console.log('socket client connected');
-      console.log(req.user);
-
+      const user = req.user;
+      const room = req.room;
+      rooms.set(user, { user, room, ws });
       ws.on('message', (data) => {
         createChat(data, '기석', 1);
-        this.wss.clients.forEach(function each(client) {
-          if (client !== ws && client.readyState === Websocket.OPEN) {
-            client.send(`${data}`);
-          }
-        });
+        msgSender(rooms.get(user), data);
+        // this.wss.clients.forEach(function each(client) {
+        //   if (client !== ws && client.readyState === Websocket.OPEN) {
+        //     client.send(`${data}`);
+        //   }
+        // });
       });
     });
   }
@@ -69,6 +75,20 @@ function getSocketWS() {
   return socket.ws;
 }
 
+function msgSender(identify, message) {
+  return new Promise((resolve, reject) => {
+    for (let target of rooms.entries()) {
+      //방 목록 객체를 반복문을 활용해 발송
+      if (identify.room == target[1].room) {
+        //같은방에 있는 사람이면 전송
+        if (identify.ws !== target[1].ws) {
+          target[1].ws.send(message);
+        }
+      }
+    }
+    resolve('succ');
+  });
+}
 module.exports = {
   initSocket,
   getSocketWS,
