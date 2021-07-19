544c24a70ca3b1436d2fc576e539b24373d1c747
jonghopark95 <jong951005@gmail.com>
2021-07-19 20:08:07 +0900

    chore : Login API 추가

--- client/src/apis/authAPI.js
+++ client/src/apis/authAPI.js
@@ -1,1 +1,17 @@
-console.log();
+const BASE_URL = 'http://localhost:8080';
+
+const AuthAPI = {};
+
+AuthAPI.requestLogin = (data) => {
+  return fetch(`${BASE_URL}/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .catch((err) => err);
+};
+
+export default AuthAPI;
