6f428cf15193d68859d9492fb048c85c16192959
Kwongiseok <ri2377@naver.com>
2021-07-19 14:43:34 +0900

    feat : product가 없을 때 404 호출

--- server/data/product.js
+++ server/data/product.js
@@ -1,7 +1,7 @@
 const { pool } = require('../db');
 
 async function getProductSeller(productid) {
-  return pool.execute(`SELECT userId FROM PRODUCT WHERE id=?`, [productid]);
+  return pool.execute(`SELECT userId FROM PRODUCT WHERE id=?`, [productid]).then((res) => res[0][0]);
 }
 
 module.exports = {
--- server/router/chatRouter.js
+++ server/router/chatRouter.js
@@ -5,10 +5,15 @@ const { getProductSeller } = require('../data/product');
 const { checkToken } = require('../middleware/checkToken');
 const router = express.Router();
 
-router.get('/:productId', checkToken(), async (req, res) => {
+// TODO: Token 미들웨어 추가할 예정
+router.get('/:productId', async (req, res) => {
   const { productId } = req.params;
+  req.user = 144;
   const productSeller = await getProductSeller(productId);
-  if (productSeller !== req.user) {
+  if (!productSeller) {
+    res.sendStatus(404);
+  }
+  if (productSeller.userId !== req.user) {
     // seller가 아닌경우
     res.sendStatus(403);
   } else {
