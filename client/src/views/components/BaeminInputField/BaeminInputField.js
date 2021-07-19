3f3b66a933eb5a6e60a3c37270758f95e9a87ad5
jonghopark95 <jong951005@gmail.com>
2021-07-19 20:09:38 +0900

    chore : Login fieldset 에러 메시지 prop으로 받지 않도록 수정

--- client/src/views/components/BaeminInputField/BaeminInputField.js
+++ client/src/views/components/BaeminInputField/BaeminInputField.js
@@ -1,13 +1,14 @@
 import { createDOMwithSelector } from '../../../utils/createDOMwithSelector.js';
 import BaeminInput from '../BaeminInput/BaeminInput.js';
 
-export default function BaeminInputField({ $selector, type, label, placeholder, errorMessage, onChange }) {
+export default function BaeminInputField({ $selector, type, label, placeholder, onChange }) {
   this.$BaeminInputField = createDOMwithSelector('div', '.baemin-input-field');
   this.$BaeminLabel = createDOMwithSelector('label', '.baemin-input-field__label');
   this.$ErrorMessage = createDOMwithSelector('small', '.baemin-input-field__error-msg');
+  this.$ErrorMessage.classList.add('hide');
 
   this.$BaeminLabel.innerText = label;
-  this.$ErrorMessage.innerText = errorMessage;
+  this.$ErrorMessage.innerText = null;
 
   this.render = () => {
     $selector.appendChild(this.$BaeminInputField);
