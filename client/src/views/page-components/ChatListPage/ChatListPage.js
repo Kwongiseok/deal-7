eb0231788f747dbc2c75b1c4d4c07e47fa478cb0
Kwongiseok <ri2377@naver.com>
2021-07-20 00:16:45 +0900

    chore : util 함수 테스트

--- client/src/views/page-components/ChatListPage/ChatListPage.js
+++ client/src/views/page-components/ChatListPage/ChatListPage.js
@@ -1,8 +1,22 @@
 import { getChatRoomsAboutProduct, getMyAllChatRooms } from '../../../apis/chatAPI.js';
+import { checkUserLoginStatus } from '../../../utils/checkUserLoginStatus.js';
 import ChatListPageHeader from './ChatListPageHeader/ChatListPageHeader.js';
 import ChatRoomList from './ChatRoomList/ChatRoomList.js';
 
 export default function ChatListPage() {
+  checkUserLoginStatus.then(({ isLoggedIn, res }) => {
+    if (!isLoggedIn) return;
+    this.setUserState({
+      isLoggedIn: true,
+      user: {
+        accessToken: res.token.accessToken,
+        name: res.userDataRows[0].name,
+        town: JSON.parse(res.userDataRows[0].town),
+      },
+    });
+  });
+
+  this.userState = {};
   this.state = [];
   this.$chatListPage = document.querySelector('#root');
 
@@ -13,6 +27,9 @@ export default function ChatListPage() {
   this.setState = (nextState) => {
     this.state = nextState;
     chatRoomList.setState(nextState);
+  };
+  this.setUserState = (userState) => {
+    this.userState = userState;
   };
 
   window.onload = () => {
