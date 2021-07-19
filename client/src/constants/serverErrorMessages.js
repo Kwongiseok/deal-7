8d609be9d214326bcb4575e1f74beaf9757eeaa1
jonghopark95 <jong951005@gmail.com>
2021-07-19 20:08:40 +0900

    chore : Error Message 추가

---
+++ client/src/constants/serverErrorMessages.js
@@ -0,0 +1,6 @@
+export const serverErrorMessages = {};
+
+// ID
+serverErrorMessages.RECEIVE_INVALID_ID = '아이디는 영문, 숫자 조합으로 20자 이하여야 해요.';
+serverErrorMessages.RECEIVE_NOT_EXIST_ID = '아이디가 존재하지 않아요.';
+serverErrorMessages.RECEIVE_EXISTED_ID = '존재하는 아이디예요.';
