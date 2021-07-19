dcd954a6dc53a48c1b4382fa7e973fd0618f13e8
jonghopark95 <jong951005@gmail.com>
2021-07-19 21:20:07 +0900

    chore : Signup API 추가

--- client/src/apis/authAPI.js
+++ client/src/apis/authAPI.js
@@ -14,4 +14,16 @@ AuthAPI.requestLogin = (data) => {
     .catch((err) => err);
 };
 
+AuthAPI.requestSignup = (data) => {
+  return fetch(`${BASE_URL}/auth/signup`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .catch((err) => err);
+};
+
 export default AuthAPI;
